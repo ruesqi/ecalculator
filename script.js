ff1fa66a2b4b7209f565226585e1cafceafa7546
Rue <viholrutu2000@gmail.com>
2024-11-22 15:28:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -57,29 +57,3 @@ function deleteLast() {
         inputField.value = currentValue.slice(0, -1);
     }
 }
-
-function sendCalculation() {
-    const inputField = document.getElementById('input_expression');
-    const expression = inputField.value;
-
-    // Send the expression to the external server (eternitycalculatorteamo.co)
-    fetch('https://eternitycalculatorteamo.co/calculate', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer pk1_83df53988e8796f23db912b80c0d0930a41805ec361d9571a1b76ab5a581b46a' // Use the Public Key
-        },
-        body: JSON.stringify({ expression: expression }), // Send the input as JSON data
-    })
-        .then(response => response.json()) // Parse the response as JSON
-        .then(data => {
-            if (data.result !== undefined) {
-                alert('Result: ' + data.result); // Display the result to the user
-            } else {
-                alert('Error: ' + data.error); // Display an error if something went wrong
-            }
-        })
-        .catch(error => {
-            alert('There was an error connecting to the server: ' + error); // Handle connection errors
-        });
-}
